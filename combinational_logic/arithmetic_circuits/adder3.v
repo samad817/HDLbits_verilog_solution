0c772fbd68c757b85c55b55431b0e238bc6d9c65
samad817 <samad2006abdl@gmail.com>
2026-07-07 11:58:44 +0530

    feat: solve structural 3-bit ripple carry adder

---
+++ combinational_logic/arithmetic_circuits/adder3.v
@@ -0,0 +1,19 @@
+module top_module( 
+    input [2:0] a, b,
+    input cin,
+    output [2:0] cout,
+    output [2:0] sum );
+    
+    adder bit0(.a(a[0]),.b(b[0]),.cin(cin),.cout(cout[0]),.sum(sum[0]));
+    adder bit1(.a(a[1]),.b(b[1]),.cin(cout[0]),.cout(cout[1]),.sum(sum[1]));
+    adder bit2(.a(a[2]),.b(b[2]),.cin(cout[1]),.cout(cout[2]),.sum(sum[2]));
+
+endmodule
+
+module adder (input a,b,cin,
+              output cout,sum );
+    
+  assign sum = a ^ b ^ cin;
+    assign cout = a & b|b & cin|cin & a;
+    
+endmodule
