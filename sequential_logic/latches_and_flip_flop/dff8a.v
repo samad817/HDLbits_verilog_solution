c75d9de3a4003a133b7c3c9e7cb96bdec57d5f27
samad817 <samad2006abdl@gmail.com>
2026-07-09 14:32:11 +0530

    feat: solve 8-bit dff with active-high asynchronous reset

---
+++ sequential_logic/latches_and_flip_flop/dff8a.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always @(posedge clk or posedge areset) begin
+        if(areset) begin
+            q <= 1'b0;
+        end
+        else begin
+            q <=d;
+        end
+    end
+
+endmodule
