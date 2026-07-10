d723509c622e51a2a53257a4658970107f31b8f0
samad817 <samad2006abdl@gmail.com>
2026-07-10 11:55:05 +0530

    feat: solve 8-bit positive edge detector circuit

---
+++ sequential_logic/latches_and_flip_flop/edgedetect.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    wire [7:0] last;
+    always @(posedge clk) begin
+        last <= in;
+        pedge <= in & ~last;
+    end
+
+endmodule
