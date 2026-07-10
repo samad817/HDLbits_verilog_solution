67d4bcaa5570e740df20b070563418a802ba3d5a
samad817 <samad2006abdl@gmail.com>
2026-07-10 12:00:10 +0530

    feat: solve 32-bit sticky negative-edge capture register

---
+++ sequential_logic/latches_and_flip_flop/edgecapture.v
@@ -0,0 +1,17 @@
+module top_module (
+    input clk,
+    input reset,
+    input [31:0] in,
+    output [31:0] out
+);
+     wire  [31:0] in_last; 
+    always @(posedge clk) begin
+        in_last <= in;       
+        if (reset) begin
+            out <= 32'b0;
+        end 
+        else begin
+            out <= out | (~in & in_last);
+        end
+    end
+endmodule
