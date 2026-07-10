8222529b4f453909d03508bb12b3db33df6e3911
samad817 <samad2006abdl@gmail.com>
2026-07-10 11:56:07 +0530

    feat: solve 8-bit dual edge detector using bitwise xor

---
+++ sequential_logic/latches_and_flip_flop/edgedetect3.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    wire [7:0] last;
+    always @(posedge clk) begin
+        last <= in;
+        anyedge <= in ^ last;
+    end
+
+endmodule
